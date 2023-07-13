adb06f2ceb2429e454ec0003ab2c072075b9b933
Trisha9987 <138782760+Trisha9987@users.noreply.github.com>
2023-07-13 12:31:15 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    API_ID = int(os.environ.get("API_ID", "29794930"))
+    API_HASH = os.environ.get("API_HASH", "2713ab0e7bf2c21f6cc590625920def7")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6349330284:AAGkzAq9Skio25m2VpmKOlUy7urvXMstz4k")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOHIBu4bCGyx6iHI3fjGEBg_DhrUeKoDBZU2z8MYcqyrrUA0BcEHssP-7mAFqLlF4pNZOpMZloRKLuIWyQM0iFElN_lZ93oD3tqZVzzOhDwyAhgmfXnAjNBkQWHdHyWq8NnMiWFhCHFZZDr8vXWKNPwDjLaapna9X44MpJ2MpeO6zFQEpPcRmLwsBrW71kfDJ4ppxkguZ32bjwPySvP659UTFExcrX3YVWxkpXDV50ONCIn55BRH_EcTeS1qIhHrGk1vOgxevedIaLIKOtbA5hTUCP11TAfJB225-X14CzF6PZKLCtSiNi0aY6hoxSLXONEi_ORYkOX9oUDI8zMnyh3xVM3A=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Megha_Chander_MusicBot")
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/612b4483b2ed270c4070a.jpg")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
-    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "")) # telegram I'd not Username
+    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "6391769382")) # telegram I'd not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
     AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', None) # Change it to "True"
